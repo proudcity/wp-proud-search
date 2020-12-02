c1514ba28b7db4a6f98feff985f19fde433961e4
Alex Schmoe <aschmoe@gmail.com>
2020-12-01 21:42:57 -0800

    platform: search results page heading structure #1779 https://github.com/proudcity/wp-proudcity/issues/1779

--- templates/search-page.php
+++ templates/search-page.php
@@ -1,4 +1,4 @@
 <?php the_widget('SearchBox'); ?>
 <?php echo apply_filters( 'proud_search_page_message', '' ); ?>
-<h3>Results</h3>
+<h2 class="h3">Results</h2>
 <?php $search_results->print_list(); ?>
