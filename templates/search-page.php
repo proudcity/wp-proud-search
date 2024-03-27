4dddbf93de92d5eb199bfc4ac069f461d485bebc
Curtis McHale <curtis@curtismchale.ca>
2024-03-27 10:00:33 -0700

    adding number of results returned

--- templates/search-page.php
+++ templates/search-page.php
@@ -1,4 +1,4 @@
-<?php the_widget('SearchBox'); ?>
+<?php the_widget('SearchBox'); ?><!-- templates/search-page.php -->
 <?php echo apply_filters( 'proud_search_page_message', '' ); ?>
-<h2 class="h3">Results</h2>
+<h2 class="h3"><?php echo absint( $search_results->get_total_found_posts() ); ?> Results Found</h2>
 <?php $search_results->print_list(); ?>
